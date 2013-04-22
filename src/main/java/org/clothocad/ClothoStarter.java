7bcec7cc0bba60c573fff571ce3a3b6d68ba5c46
Ernst Oberortner <e.oberortner@gmail.com>
2013-04-22 09:18:35 -0400

    node.js clotho client

--- src/main/java/org/clothocad/ClothoStarter.java
+++ src/main/java/org/clothocad/ClothoStarter.java
@@ -7,11 +7,13 @@ public class ClothoStarter {
 	public static void main(String[] args) 
 			throws Exception {
 
+		// start the message broker
 		new ClothoBroker();
 		
 		// wait a bit until the broker is running
-		Thread.sleep(2000);
+		Thread.sleep(4000);
 		
+		// now, start the core (i.e. the server)
 		new ClothoCore().start();	
 		
 		Object lock = new Object();
