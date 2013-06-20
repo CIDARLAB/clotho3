ca65acfe6f10c6d72d60874da6ec8798a4f80fd2
J. Christopher Anderson <jcanderson2167@gmail.com>
2013-06-19 22:24:41 -0700

    Revert "Instance test for Stephanie"
    This reverts commit dbfb43c1ad3a0069d257dcd9e34c3584b0b460c4.

--- src/main/java/org/clothocad/core/aspects/Proctor/TemplatePaver.java
+++ src/main/java/org/clothocad/core/aspects/Proctor/TemplatePaver.java
@@ -4,13 +4,9 @@
  */
 package org.clothocad.core.aspects.Proctor;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  *
@@ -22,19 +18,9 @@ public class TemplatePaver extends Paver {
     public TemplatePaver(String paver_title, String template) {
         super(paver_title, "template");
         this.template = template;
-        testObj = new JSONObject();
-        try {
-            testObj.put("testy", template);
-        } catch (JSONException ex) {
-            Logger.getLogger(TemplatePaver.class.getName()).log(Level.SEVERE, null, ex);
-        }
     }
 
     @Getter
     @Setter
     private String template;
-    
-    @Getter
-    @Setter
-    private JSONObject testObj;
 }
--- src/main/java/org/clothocad/model/Trail.java
+++ src/main/java/org/clothocad/model/Trail.java
@@ -5,12 +5,16 @@
 package org.clothocad.model;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Setter;
 import org.clothocad.core.aspects.Proctor.Module;
 import org.clothocad.core.datums.Sharable;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
