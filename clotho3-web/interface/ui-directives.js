57509158b0824a11395b3222d03e586cc144390b
Maxwell Bates <maxbates@gmail.com>
2013-07-07 17:18:31 -0700

    start draggable directive

--- clotho3-web/interface/ui-directives.js
+++ clotho3-web/interface/ui-directives.js
@@ -634,3 +634,137 @@ Application.Interface.directive( 'tooltipHtmlUnsafePopup', function () {
 Application.Interface.directive( 'tooltipHtmlUnsafe', [ '$tooltip', function ( $tooltip ) {
     return $tooltip( 'tooltipHtmlUnsafe', 'tooltip', 'mouseenter' );
 }]);
+
+/***************
+    DRAG N DROP
+ ***************/
+
+/*
+ * angular-dragon-drop v0.0.1
+ * (c) 2013 Brian Ford http://briantford.com
+ * License: MIT
+ *
+ * todo - clicking sometimes causes deletion of element
+ * note - replaces ngRepeat in form draggable="item in collection"
+ */
+
+
+Application.Interface.directive('uiDrag', function ($document, $compile) {
+
+        var dragValue,
+            dragOrigin,
+            floaty;
+
+        var drag = function (ev) {
+            var x = ev.clientX,
+                y = ev.clientY;
+
+            floaty.css('left', x + 10 + 'px');
+            floaty.css('top', y + 10 + 'px');
+        };
+
+        // todo - use class unselectable (in base.css)
+        var disableSelect = function () {
+            angular.element(document.body).addClass('unselectable');
+        };
+
+        var enableSelect = function () {
+            angular.element(document.body).removeClass('unselectable');
+        };
+
+        return {
+            restrict: 'A',
+            terminal: true,
+            link: function (scope, elt, attr) {
+
+                // get the `thing in things` expression
+                console.log(attr);
+                console.log(elt);
+                var expression = attr.uiDrag;
+                var match = expression.match(/^\s*(.+)\s+in\s+(.*?)\s*$/);
+                if (!match) {
+                    throw Error("Expected ngRepeat in form of '_item_ in _collection_' but got '" +
+                        expression + "'.");
+                }
+                var lhs = match[1];
+                var rhs = match[2];
+
+                //todo -this sucks - rewrite
+                // pull out the template to re-use. Improvised ng-transclude.
+                var template = elt.html();
+                elt.html('');
+                var child = angular.element('<div ng-repeat="' + lhs + ' in ' + rhs + '">' + template + '</div>');
+                elt.append(child);
+
+                //copy over class
+                elt.removeAttr('class');
+                child.addClass(attr.class);
+
+                $compile(child)(scope);
+
+                var spawnFloaty = function () {
+                    scope.$apply(function () {
+                        floaty = angular.element('<div style="position: fixed;">' + template + '</div>');
+                        var floatyScope = scope.$new();
+                        floatyScope[lhs] = dragValue;
+                        $compile(floaty)(floatyScope);
+                        angular.element(document.body).append(floaty);
+                    });
+
+                    $document.bind('mousemove', drag);
+                };
+
+                var killFloaty = function () {
+                    $document.unbind('mousemove', drag);
+                    if (floaty) {
+                        floaty.remove();
+                        floaty = null;
+                    }
+                };
+
+                elt.bind('mousedown', function (ev) {
+                    if (dragValue) {
+                        return;
+                    }
+                    scope.$apply(function () {
+                        var targetScope = angular.element(ev.target).scope();
+                        var value = dragValue = targetScope[lhs];
+                        //console.log(value);
+                        var list = scope.$eval(rhs);
+                        dragOrigin = list;
+                        list.splice(list.indexOf(value), 1);
+                    });
+                    spawnFloaty();
+                    drag(ev);
+                    disableSelect();
+
+                });
+
+                // handle something being dropped here
+                elt.bind('mouseup', function (ev) {
+                    if (dragValue) {
+                        scope.$apply(function () {
+                            var list = scope.$eval(rhs);
+                            list.push(dragValue);
+                            dragValue = dragOrigin = null;
+                        });
+                    }
+                    enableSelect();
+                    killFloaty();
+                });
+
+                // else, the event bubbles up to document
+                $document.bind('mouseup', function (ev) {
+                    if (dragValue) {
+                        scope.$apply(function () {
+                            dragOrigin.push(dragValue);
+                            dragValue = dragOrigin = null;
+                        });
+                        enableSelect();
+                        killFloaty();
+                    }
+                });
+
+            }
+        };
+    });
