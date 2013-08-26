04ddbf14e11f7ed61c40df1e7f23802d6d1c9fde
Stephanie Paige <stmpaige@gmail.com>
2013-08-26 00:35:34 -0400

    Converted webserver to servlets
    Requires connection on port 8443 now

--- src/main/java/org/clothocad/webserver/jetty/ClothoWebserver.java
+++ src/main/java/org/clothocad/webserver/jetty/ClothoWebserver.java
@@ -1,5 +1,6 @@
 package org.clothocad.webserver.jetty;
 
+import com.google.inject.servlet.GuiceFilter;
 import java.security.KeyStore;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -8,17 +9,15 @@ import lombok.Getter;
 
 import org.clothocad.core.layers.communication.connection.ws.ClothoWebSocket;
 import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocketHandler;
-import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.server.ssl.SslSelectChannelConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.websocket.WebSocketServlet;
 
 public class ClothoWebserver {
 
@@ -35,53 +34,43 @@ public class ClothoWebserver {
         //Connectors
 
         SelectChannelConnector connector0 = new SelectChannelConnector();
-        connector0.setPort(8080);
+        connector0.setPort(nPort);
         connector0.setMaxIdleTime(30000);
         connector0.setRequestHeaderSize(8192);
 
         SslSelectChannelConnector ssl_connector = new SslSelectChannelConnector();
-        ssl_connector.setPort(8443);
+        ssl_connector.setPort(8443); //TODO: make configurable
         SslContextFactory cf = ssl_connector.getSslContextFactory();
         cf.setKeyStore(keystore);
         server.setConnectors(new Connector[]{connector0, ssl_connector});
+        
+        //Websocket
 
-        /**
-         * for WEB resources (HTML, CSS, JavaScript etc.) *
-         */
-        ResourceHandler resHandler = new ResourceHandler();
-        resHandler.setDirectoriesListed(true);
+        WebSocketServlet wsServlet = new WebSocketServlet() {
 
-        //Regardless of repository, the URI to access is http://localhost:8080/#/
-        //Set directory to clotho3-web (this repository)
-        resHandler.setWelcomeFiles(new String[]{"index.html"});
-        resHandler.setResourceBase("./clotho3-web/");
-
-        /**
-         * Clotho3.0 Java Websocket *
-         */
-        System.out.println("Ernst (not for sb6.0 demo, but eventually), this should be pulling from DB namespaced by the View's uuid to avoid collisions in naming.  Not in flatfiles");
-        WebSocketHandler wsHandler = new WebSocketHandler() {
             @Override
             public WebSocket doWebSocketConnect(HttpServletRequest request, String protocol) {
-                return new ClothoWebSocket(request.getSession().getId());
+                return new ClothoWebSocket(request.getSession().getId()); //todo - can inject router here w/ guice
             }
+            
         };
-
-        ContextHandler contextHandler = new ContextHandler();
-        contextHandler.setContextPath("/websocket");
-        contextHandler.setHandler(wsHandler);
-
-        /**
-         * Session Handling **
-         */
-        SessionHandler sessionHandler = new SessionHandler();
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{
-            sessionHandler,
-            resHandler,
-            wsHandler});
-        server.setHandler(handlers);
+        
+        //Static resources
+        
+        DefaultServlet staticServlet = new DefaultServlet();
+        
+        //Handler stack
+        
+        ServletContextHandler servletHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        servletHandler.setContextPath("/");
+        servletHandler.setResourceBase("./clotho3-web/");
+        servletHandler.setWelcomeFiles(new String[]{"index.html"});
+        
+        servletHandler.addFilter(GuiceFilter.class, "/*", null);
+        servletHandler.addServlet(new ServletHolder(staticServlet), "/*");
+        servletHandler.addServlet(new ServletHolder(wsServlet), "/websocket");
+        
+        server.setHandler(servletHandler);
 
         server.start();
     }
