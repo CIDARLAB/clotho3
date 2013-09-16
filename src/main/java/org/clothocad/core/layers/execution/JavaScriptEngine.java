644d20ac0c8cc4299e610dc0afcceceb0cb0607a
Stephanie Paige <stmpaige@gmail.com>
2013-09-16 04:33:15 -0400

    Global window var hack for demo

--- src/main/java/org/clothocad/core/layers/execution/JavaScriptEngine.java
+++ src/main/java/org/clothocad/core/layers/execution/JavaScriptEngine.java
@@ -35,6 +35,8 @@ class JavaScriptEngine extends AbstractScriptEngine implements HackEngine, Invoc
 
         FunctionObject f = new JSLoader(this).getLoadFunction();
         stdObjects.put("load", stdObjects, f);
+        cx.evaluateString(stdObjects, "var window = {};", "window hack", 1, null);
+        
         try {
             cx.evaluateReader(stdObjects, new FileReader("clotho3-web/lib/lodash.js"), "lodash.js", 1, null);
         } catch (FileNotFoundException ex) {
