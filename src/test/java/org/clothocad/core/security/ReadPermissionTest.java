8690fac6a32fc1b6f786031dad2d8aef4b91a633
zhaobo <zhaoboyu@bu.edu>
2013-11-14 15:57:16 -0500

    security test cases. none, read, write, owner permissions on public/private objects

---
+++ src/test/java/org/clothocad/core/security/ReadPermissionTest.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.clothocad.core.security;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.UnavailableSecurityManagerException;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.*;
+
+import org.apache.shiro.subject.Subject;
+import org.clothocad.core.communication.Router;
+import org.clothocad.core.communication.ServerSideAPI;
+import org.clothocad.core.execution.Mind;
+import org.clothocad.core.persistence.Persistor;
+import org.clothocad.core.persistence.mongodb.MongoDBModule;
+import org.clothocad.core.testers.ClothoTestModule;
+import org.clothocad.core.util.SecurityTestUtils;
+import org.junit.Test;
+
+/**
+ *
+ * @author yu
+ */
+public class ReadPermissionTest {
+
+    /**
+     *
+     */
+    private Router router;
+    private Persistor persistor;
+    private String requestId;
+    private Mind mind;
+    private Injector injector;
+    private ServerSideAPI api;
+    private SecurityTestUtils util;
+
+    public ReadPermissionTest() {
+    }
+
+    public void initAPI(String id) {
+        injector = Guice.createInjector(new ClothoTestModule(), new MongoDBModule());
+        persistor = injector.getInstance(Persistor.class);
+        //ServerSideAPI api = new DummyAPI(persistor);
+        api = new ServerSideAPI(null, persistor, null, id);
+        util=new SecurityTestUtils(persistor);
+    }
+
+    public Map<String, Object> initObj() {
+        Map<String, Object> defObj = new HashMap<>();
+        Subject currentUser = SecurityUtils.getSubject();
+        //not sure if this is right way
+        currentUser.checkPermission(util.credentials.get("owner"));
+        defObj.put("private", util.objects.get("private"));
+        
+        UsernamePasswordToken token = new UsernamePasswordToken("owner", "owner");
+        currentUser.login(token);
+        api.login("owner", "owner");
+        api.create(defObj);
+        api.logout();
+        return defObj;
+    }
+
+    /*
+     * no exception expected
+     */
+    @Test
+    public void testRead() {
+        initAPI("0000");
+        Map<String, Object> newObj = initObj();
+
+        try {
+            Subject currentUser = SecurityUtils.getSubject();
+           
+            UsernamePasswordToken token = new UsernamePasswordToken("read", "read");
+            currentUser.login(token);
+            currentUser.checkPermission(util.credentials.get("read"));
+            token.setRememberMe(true);
+            api.login("read", "read");
+            api.get(newObj);
+        } catch (UnavailableSecurityManagerException e) {
+        }
+
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void testEdit() {
+        initAPI("0001");
+        Map<String, Object> newObj = initObj();
+
+        try {
+            Subject currentUser = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken("read", "read");
+            currentUser.login(token);
+            currentUser.checkPermission(util.credentials.get("read"));
+            token.setRememberMe(true);
+            api.login("read", "read");
+            api.set(newObj);
+        } catch (UnavailableSecurityManagerException e) {
+        }
+
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void testDelete() {
+        initAPI("0002");
+        Map<String, Object> newObj = initObj();
+
+        try {
+            Subject currentUser = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken("read", "read");
+            currentUser.login(token);
+            currentUser.checkPermission(util.credentials.get("read"));
+            token.setRememberMe(true);
+            api.login("read", "read");
+            api.destroy(newObj);
+        } catch (UnavailableSecurityManagerException e) {
+        }
+
+    }
+
+    @Test(expected = UnauthorizedException.class)
+    public void testEditPermission() {
+        initAPI("0003");
+        Map<String, Object> newObj = initObj();
+
+        try {
+            Subject currentUser = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken("unauth", "unauth");
+            currentUser.login(token);
+            currentUser.checkPermission(util.credentials.get("read"));
+            token.setRememberMe(true);
+            api.login("unauth", "unauth");
+            /*
+             *code here to edit permission 
+             */
+
+        } catch (UnavailableSecurityManagerException e) {
+        }
+
+    }
+}
