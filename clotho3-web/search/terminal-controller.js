5058105c06ed7c2cff5f34cf409f7ae37c632f2f
Maxwell Bates <maxbates@gmail.com>
2013-11-17 16:57:48 -0800

    trail update (in progress)

--- clotho3-web/search/terminal-controller.js
+++ clotho3-web/search/terminal-controller.js
@@ -1,7 +1,4 @@
-// NOTE  --- PRESENT IN Command Bar DIRECTIVES (THIS IS FOR TESTING)
 
 Application.Search.controller('TerminalCtrl', ['$scope', 'Clotho', 'Searchbar', '$location', function($scope, Clotho, Searchbar, $location) {
     $scope.log = Searchbar.log;
-
-
 }]);
