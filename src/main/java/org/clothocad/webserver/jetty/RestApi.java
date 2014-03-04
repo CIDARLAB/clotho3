f07787eb1141a1267cb4f75b829f5eb77a31ec9a
MosNicholas <mosnicholas@berkeley.edu>
2014-03-04 01:34:45 -0800

    added some login functionality -- untested

--- src/main/java/org/clothocad/webserver/jetty/RestApi.java
+++ src/main/java/org/clothocad/webserver/jetty/RestApi.java
@@ -2,7 +2,6 @@ package org.clothocad.webserver.jetty;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Enumeration;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -11,6 +10,7 @@ import org.clothocad.core.communication.*;
 import org.clothocad.core.persistence.Persistor;
 import org.clothocad.core.execution.Mind;
 import org.clothocad.core.persistence.mongodb.MongoDBModule;
+import org.apache.shiro.SecurityUtils;
 // import org.clothocad.core.testers.ClothoTestModule;
 // This is in the test package
 
@@ -23,20 +23,18 @@ import javax.servlet.http.HttpServletResponse;
 public class RestApi extends HttpServlet {
 
     private static ServerSideAPI api;
-    private static ServerSideAPI unprivilegedUser;
     private static Persistor persistor;
     private static Mind mind;
     private static Router router;
 
-    private void createAPI() {
-        // Injector injector = Guice.createInjector(new ClothoTestModule(), new MongoDBModule());
-        // persistor = injector.getInstance(Persistor.class);
-        // router = injector.getInstance(Router.class);
-        // mind = new Mind();
-        // api = new ServerSideAPI(mind, persistor, router, null);
-        // persistor.connect();
-        // mind.setConnection(new TestConnection("test"));
-    }
+    // retrieve mind object/create new one - session id w/ shiro
+    // minds are in router
+
+    // http://shiro.apache.org/
+    // 2 methods:
+    // pass request to serversideapi w/ mind obj
+    // get : shareable through uuid
+    // post : change value of shareable with new val
 
     protected void doGet(HttpServletRequest request, 
     	HttpServletResponse response) throws ServletException, IOException {
@@ -45,61 +43,35 @@ public class RestApi extends HttpServlet {
 
     	String path = request.getPathInfo();
 
-        switch (path) {
-            // Needed to add possibility of having trailing slash.
-            case "/autocomplete/":
-            case "/autocomplete":
-                String paramUserText = (String) request.getParameter("userText");
-                if (paramUserText != null) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println("<h1>autocomplete</h1>");
-                } else {
-                    response.sendError(response.SC_BAD_REQUEST, "Parameter userText required");
-                }
-                break;
-            case "/autocompleteDetail/":
-            case "/autocompleteDetail":
-                String paramUUID = (String) request.getParameter("uuid");
-                if (paramUUID != null) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println("<h1>autocompleteDetail</h1>");
-                } else {
-                    response.sendError(response.SC_BAD_REQUEST, "Parameter uuid required");
-                }
-                break;
-            case "/submit/":
-            case "/submit":
-                String paramCommand = (String) request.getParameter("command");
-                if (paramCommand != null) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println("<h1>submit</h1>");
-                } else {
-                    response.sendError(response.SC_BAD_REQUEST, "Parameter command required");
-                }
-                break;
-            case "/learn/":
-            case "/learn":
-                // Need to deal with other case of learn where an object is passed in to SSA
-                String paramUI = (String) request.getParameter("userInput");
-                String paramCommand1 = (String) request.getParameter("command");
-                if (paramUI != null && paramCommand1 != null) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println("<h1>learn</h1>");
-                } else {
-                    response.sendError(response.SC_BAD_REQUEST, "Parameter command & userInput required");
-                }
-                break;
-            case "/login/":
-            case "/login":
-                String paramUName = (String) request.getParameter("username");
-                String paramPass = (String) request.getParameter("password");
-                if (paramUName != null && paramPass != null) {
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println("<h1>login</h1>");
-                } else {
-                    response.sendError(response.SC_BAD_REQUEST, "Parameter username & password required");
-                }
-                break;
+        if (path == "get/trial/") {
+            if (SecurityUtils.getSubject().isAuthenticated()) {
+                String username = SecurityUtils.getSubject().getPrincipal().toString();
+                mind.setUsername(username);
+                persistor.save(mind);
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("You are logged in");
+            } else {
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("You are not logged in");
+            }
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, 
+        HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/json");
+
+        String path = request.getPathInfo();
+
+        if (path == "post/trial/") {
+            if (SecurityUtils.getSubject().isAuthenticated()) {
+                String username = SecurityUtils.getSubject().getPrincipal().toString();
+                mind.setUsername(username);
+                persistor.save(mind);
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("<h1>autocomplete</h1>");
+            }
         }
     }
 }
