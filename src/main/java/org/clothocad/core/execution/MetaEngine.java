8b0f5be7b35ff82783fcc7cf34f085c77c05a834
Stephanie Paige <stmpaige@gmail.com>
2014-03-23 22:23:42 -0400

    Modifying MetaEngine for Jackson

--- src/main/java/org/clothocad/core/execution/MetaEngine.java
+++ src/main/java/org/clothocad/core/execution/MetaEngine.java
@@ -4,6 +4,8 @@
  */
 package org.clothocad.core.execution;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.util.StdConverter;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -25,70 +27,62 @@ import org.mozilla.javascript.RhinoException;
  * @author spaige
  */
 public class MetaEngine {
-    
-    private transient Map<Language,ScriptContext> contexts = new HashMap<>();
-    
-    private transient Map<Language,ScriptEngine> engines = new HashMap<>();
-    
-    private Map<Language, Bindings> bindings = new HashMap<>();
-    
 
-    /*
-    @PreLoad
-    public DBObject preLoad(DBObject obj){
-        Map map = ((BasicDBObject) obj.get("bindings")).toMap();
-        for (Object olang : ((BasicDBObject) obj.get("bindings")).toMap().keySet()){
-            Language language = Language.valueOf(olang.toString());
-            BasicDBObject b = (BasicDBObject) map.get(olang);
-            bindings.put(language, b.toMap());
+    private transient Map<Language, ScriptContext> contexts = new HashMap<>();
+    private transient Map<Language, ScriptEngine> engines = new HashMap<>();
+    
+    @JsonSerialize(converter = BindingsMapConverter.class)
+    private Map<Language, Bindings> bindings = new HashMap<>();
+
+    public static class BindingsMapConverter extends StdConverter<Map<Language,Bindings>,Map>{
+
+        @Override
+        public Map<Language,Bindings> convert(Map<Language,Bindings> value) {
+            for (Language key : value.keySet()){
+                Bindings bindings = value.get(key);
+                //api does not need to be serialized, and does not serialize cleanly
+                bindings.remove("clotho");
+            }
+            return value;
         }
-        obj.removeField("bindings");
-        return obj;
+        
     }
-    * XXX: create contexts after load
-    @PostLoad
-    public void postLoad(){
-        for (Language key : bindings.keySet()){
-            ScriptContext context = getContext(key);
-            context.setBindings(new SimpleBindings(bindings.get(key)), ScriptContext.ENGINE_SCOPE);
-        }
-    }
-    * XXX: make clotho binding effectively transient
-    @PrePersist
-    public void prePersist(){
-        for (Language language : contexts.keySet()){
-            Bindings bindings = contexts.get(language).getBindings(ScriptContext.ENGINE_SCOPE);
-            bindings.remove("clotho");
-            this.bindings.put(language, bindings);
-        }
-    }
-    */
-    public Object eval(String script, Language language, ScriptAPI api) throws ScriptException{
+    
+    public Object eval(String script, Language language, ScriptAPI api) throws ScriptException {
         ScriptContext context = getContext(language);
         HackEngine engine = getEngine(language);
-        
+
         injectAPI(api, context);
-        try{
-            return engine.eval(script, context);          
-        }
-        //XXX: ew, referring to a concrete engine implementation!
-        catch (RhinoException e){
+        try {
+            return engine.eval(script, context);
+        } //XXX: ew, referring to a concrete engine implementation!
+        catch (RhinoException e) {
             //is this terrible?
             throw new ScriptException(e);
-        }
+        } 
     }
 
     private ScriptContext getContext(Language language) {
-        if (!contexts.containsKey(language)) contexts.put(language, new SimpleScriptContext());
+        if (!contexts.containsKey(language)) {
+            ScriptContext context = new SimpleScriptContext();
+            if (bindings.containsKey(language)) {
+                context.setBindings(bindings.get(language), ScriptContext.ENGINE_SCOPE);
+            } else {
+                bindings.put(language, context.getBindings(ScriptContext.ENGINE_SCOPE));
+            }
+            contexts.put(language, context);
+        }
         return contexts.get(language);
     }
 
     private HackEngine getEngine(Language language) {
-        
+
         if (!engines.containsKey(language)) {
             engines.put(language, ClothoScriptEngineManager.getEngineByLanguage(language));
             ScriptEngine engine = engines.get(language);
-            if (engine == null) throw new EngineNotFoundException();
+            if (engine == null) {
+                throw new EngineNotFoundException();
+            }
             engine.setContext(getContext(language));
         }
         return new WrappedScriptEngine(engines.get(language));
@@ -102,66 +96,66 @@ public class MetaEngine {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
     public Object invoke(String functionCode, String name, List args, ScriptAPI api) throws ScriptException {
         return invoke(functionCode, "", name, args, api);
     }
-    public static String idToName(ObjectId id){
-        return "f"+id.toString();
+
+    public static String idToName(ObjectId id) {
+        return "f" + id.toString();
     }
-    
-    public void loadAsGlobal(Module module, ScriptAPI api) throws ScriptException{
-        String name = "f"+module.getId().toString();
+
+    public void loadAsGlobal(Module module, ScriptAPI api) throws ScriptException {
+        String name = "f" + module.getId().toString();
         //TODO: check last saved date
         //if (getEngine(module.getLanguage()).getContext().getBindings(ScriptContext.ENGINE_SCOPE).containsKey(name)){
         //    return;
         //}
         HackEngine engine = getEngine(module.getLanguage());
-        
-        engine.eval("var " + name + " = " +module.getCodeToLoad());
+
+        try {
+            engine.eval("var " + name + " = " + module.getCodeToLoad(), engine.getContext());
+        } catch (ScriptException e){
+            throw new ScriptException(e.getMessage(),module.getId().toString(),e.getLineNumber(), e.getColumnNumber());
+        }
     }
-    
-    public Object invoke(Function module, List args, ScriptAPI api) throws ScriptException, NoSuchMethodException{
-       HackEngine engine = getEngine(module.getLanguage());
-       injectAPI(api, engine.getContext());
-       loadAsGlobal(module, api);
-       
-       return engine.invokeFunction(idToName(module.getId()), args.toArray());
+
+    public Object invoke(Function function, List args, ScriptAPI api) throws ScriptException, NoSuchMethodException {
+        HackEngine engine = getEngine(function.getLanguage());
+            injectAPI(api, engine.getContext());
+            loadAsGlobal(function, api);
+            return engine.invokeFunction(idToName(function.getId()), args.toArray());
     }
-    
-    
-    public Object invoke(Module module, String methodName, List args, ScriptAPI api) throws ScriptException, NoSuchMethodException{
-       HackEngine engine = getEngine(module.getLanguage());
-       injectAPI(api, engine.getContext());
-       loadAsGlobal(module, api);
-    
-       Object thiz = engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE).get(idToName(module.getId()));
-       return engine.invokeMethod(thiz, methodName, args.toArray());
+
+    public Object invoke(Module module, String methodName, List args, ScriptAPI api) throws ScriptException, NoSuchMethodException {
+        HackEngine engine = getEngine(module.getLanguage());
+            injectAPI(api, engine.getContext());
+            loadAsGlobal(module, api);
+
+            Object thiz = engine.getContext().getBindings(ScriptContext.ENGINE_SCOPE).get(idToName(module.getId()));
+            return engine.invokeMethod(thiz, methodName, args.toArray());
     }
-    
+
     //XXX: de-js-ify this!
     public Object invoke(String functionCode, String setupCode, String name, List args, ScriptAPI api) throws ScriptException {
-        try {
             HackEngine engine = getEngine(Language.JAVASCRIPT);
             ScriptContext context = engine.getContext();
-            injectAPI(api,context);
-            
+        try {
+            injectAPI(api, context);
+
             //eval dependencies
             //XXX: this is terrible terrible, change to module pattern that returns a function
             engine.eval(setupCode);
-            
-            engine.eval("var " + name+ " = " + functionCode);
-            
-            
+
+            engine.eval("var " + name + " = " + functionCode);
             return engine.invokeFunction(name, args.toArray());
-            
+
         } catch (NoSuchMethodException ex) {
             throw new ScriptException(ex);
-        }
+        } 
     }
-    
-    protected String generateDependencies(Language language, Collection<ObjectId> dependencies){
-        switch (language){
+
+    protected String generateDependencies(Language language, Collection<ObjectId> dependencies) {
+        switch (language) {
             case JAVASCRIPT:
                 return new JavaScriptScript().generateImports(dependencies);
             default:
