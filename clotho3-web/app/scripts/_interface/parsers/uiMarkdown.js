86ec490e5c53c2d45581ac39daa7d18dc2d6360f
Maxwell Bates <maxbates@gmail.com>
2014-03-30 17:16:18 -0700

    markdown as promise
    close #198

--- clotho3-web/app/scripts/_interface/parsers/uiMarkdown.js
+++ clotho3-web/app/scripts/_interface/parsers/uiMarkdown.js
@@ -1,8 +1,3 @@
-//todo - create showdown module to load via angular DI, to ensure loaded
-//future - add prettify module
-$script('bower_components/showdown/src/showdown.js');
-
-
 /**
  @description Converts markdown into HTML
  @note relies on Showdown being present: https://github.com/coreyti/showdown
@@ -21,19 +16,25 @@ $script('bower_components/showdown/src/showdown.js');
  */
 
 angular.module('clotho.interface').directive('uiMarkdown', function () {
-	var converter = new Showdown.converter();
+	var converter;
+	var showdownPromise = $clotho.extensions.mixin('bower_components/showdown/src/showdown.js').then(function() {
+		converter = new Showdown.converter();
+	});
+	
 	return {
 		restrict: 'EA',
 		link: function (scope, element, attrs) {
-			if (attrs.uiMarkdown) {
-				scope.$watch(attrs.uiMarkdown, function (newVal) {
-					var html = newVal ? converter.makeHtml(newVal) : '';
+			showdownPromise.then(function() {
+				if (attrs.uiMarkdown) {
+					scope.$watch(attrs.uiMarkdown, function (newVal) {
+						var html = newVal ? converter.makeHtml(newVal) : '';
+						element.html(html);
+					});
+				} else {
+					var html = converter.makeHtml(element.text());
 					element.html(html);
-				});
-			} else {
-				var html = converter.makeHtml(element.text());
-				element.html(html);
-			}
+				}
+			});
 		}
 	};
 });
