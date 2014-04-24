56074871ef259bb086965e06b6be5f920e9572fc
Maxwell Bates <maxbates@gmail.com>
2014-04-24 13:36:23 -0700

    trailContents directive
    close #234

---
+++ clotho3-web/app/scripts/_trails/trailContents.js
@@ -0,0 +1,21 @@
+/**
+ * @name trail-contents
+ * @type directive
+ *
+ * @description Given a trail, the directive will display its contents (Chapters, Pages)
+ */
+angular.module('clotho.trails').directive('trailContents', function(Trails) {
+
+	return {
+		restrict: 'A',
+		replace : true,
+		templateUrl : 'views/_trails/trailContents.html',
+		scope: {
+			trail: '=trailContents'
+		},
+		link: function (scope,element,attrs) {
+
+			scope.mapIcon = Trails.mapIcon;
+		}
+	}
+});
