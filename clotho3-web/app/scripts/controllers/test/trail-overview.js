3c6244da4382cab3c366659f0a588eb5f05e789e
Maxwell Bates <maxbates@gmail.com>
2014-05-05 14:26:12 -0700

    largely refactor clotho intro trail + demo new features
    address #246

--- clotho3-web/app/scripts/controllers/test/trail-overview.js
+++ clotho3-web/app/scripts/controllers/test/trail-overview.js
@@ -1,10 +1,8 @@
 'use strict';
 
 angular.module('clotho.trails')
-	.controller('TestTrailOverviewCtrl', function ($scope, Clotho) {
+	.controller('TestTrailOverviewCtrl', function ($scope, $http, Clotho) {
 		Clotho.get('bb99191e810c19729de860fe').then(function (result) {
 			$scope.trail = result;
 		});
-
-		$scope.defaultTrailIcon = 'images/trails/trails_splash.jpg';
 	});
