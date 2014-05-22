5d94fe910438a4f4556868465ebc4ddfab809c04
Maxwell Bates <maxbates@gmail.com>
2014-05-22 00:50:55 -0700

    rebuild

--- clotho3-web/app/scripts/controllers/team.js
+++ clotho3-web/app/scripts/controllers/team.js
@@ -1,6 +1,7 @@
 angular.module('clotho.webapp')
 .controller('TeamCtrl', function ($scope, Clotho) {
-	Clotho.query({ id: { $regex: 'clotho.developer.*', $options: 'i' } }).then(function (data) {
+	Clotho.query({ id: { $regex: 'clotho.developer.*', $options: 'i' } }, {mute : true})
+	.then(function (data) {
 		$scope.ClothoTeam	= data;
 	});
 });
