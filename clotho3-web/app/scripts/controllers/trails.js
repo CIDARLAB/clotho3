eb425580b0fe907d4a5d44ff4f9f8243ddff93aa
Maxwell Bates <maxbates@gmail.com>
2014-05-22 16:46:38 -0700

    update LabPerson and Trail schema references

--- clotho3-web/app/scripts/controllers/trails.js
+++ clotho3-web/app/scripts/controllers/trails.js
@@ -1,7 +1,7 @@
 angular.module('clotho.webapp').controller('TrailsCtrl', function ($scope, Clotho) {
 	$scope.trails = [];
 
-	Clotho.query({schema : "Trail"}).then(function(result) {
+	Clotho.query({schema : "org.clothocad.model.Trail"}).then(function(result) {
 		$scope.trails = result;
 	});
 });
