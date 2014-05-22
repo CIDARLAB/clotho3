43aefb4124f8dd01ce728b81deb0919c38aea546
Maxwell Bates <maxbates@gmail.com>
2014-05-22 00:45:49 -0700

    restyle hotkeys modal

--- clotho3-web/app/scripts/_interface/interaction/hotkeysGlobal.js
+++ clotho3-web/app/scripts/_interface/interaction/hotkeysGlobal.js
@@ -1,4 +1,27 @@
 angular.module('clotho.interface')
+	.config(function (hotkeysProvider) {
+		hotkeysProvider.template = '' +
+			'<div class="modal fade hotkeyModal" ng-class="{in: helpVisible}">' +
+				'<div class="modal-dialog">' +
+					'<div class="modal-content">' +
+						'<div class="modal-header">' +
+							'<button type="button" class="close" ng-click="closeCheatSheet()">&times;</button>' +
+							'<h4 class="modal-title">{{ title }}</h4>' +
+						'</div>' +
+						'<div class="modal-body">' +
+							'<table><tbody>' +
+								'<tr ng-repeat="hotkey in hotkeys | filter:{ description: \'!$$undefined$$\' }">' +
+									'<td class="hotkey-keys">' +
+									'<span ng-repeat="key in hotkey.format() track by $index" class="hotkey-key">{{ key }}</span>' +
+									'</td>' +
+									'<td class="hotkey-text">{{ hotkey.description }}</td>' +
+								'</tr>' +
+							'</tbody></table>' +
+						'</div>' +
+					'</div><!-- /.modal-content -->' +
+				'</div><!-- /.modal-dialog -->' +
+			'</div><!-- /.modal -->';
+	})
 .run(function (hotkeys, $location, CommandBar) {
 	hotkeys.add('f', 'Focus Command Bar', function (event) {
 		event.preventDefault();
