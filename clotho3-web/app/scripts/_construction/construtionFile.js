3f7331572cd1f9ef1de383e2201c5279ed3dc853
Maxwell Bates <maxbates@gmail.com>
2014-05-23 21:06:57 -0700

    create new demo construction files, update old links
    close #317

--- clotho3-web/app/scripts/_construction/construtionFile.js
+++ clotho3-web/app/scripts/_construction/construtionFile.js
@@ -6,27 +6,19 @@ angular.module('clotho.construction')
  * @restrict A
  *
  * @description
- * Visualizer for construction files
+ * Visualizer for construction files.
  */
-	.directive('constructionFile', function () {
+	.directive('constructionFile', function (ConstructionSimulator) {
 		return {
 			restrict : 'A',
 			templateUrl : 'views/_construction/constructionFile.html',
 			scope : {
 				file : '=constructionFile'
 			},
-			controller : function constructionFileController ($scope, $element, $attrs) {
-
-				this.getStep = function (index) {
-					if (angular.isEmpty($scope.file)) {
-						return null;
-					}
-					return $scope.file.steps[index];
-				};
-
-			},
 			link : function (scope, element, attrs) {
-
+				scope.$watch('file', function (newfile) {
+					scope.computed = ConstructionSimulator.process(newfile);
+				});
 			}
 		}
 	});
