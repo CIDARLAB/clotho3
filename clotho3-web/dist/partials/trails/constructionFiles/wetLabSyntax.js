07a87a97d3efdf273d4accff71f695b5bbfb3d27
Maxwell Bates <maxbates@gmail.com>
2014-06-05 12:00:52 -0700

    update build

--- clotho3-web/dist/partials/trails/constructionFiles/wetLabSyntax.js
+++ clotho3-web/dist/partials/trails/constructionFiles/wetLabSyntax.js
@@ -1,7 +1,5 @@
 'use strict';
 
-$clotho.extensions.controller('constructionFiles_wetLabSyntaxCtrl', function($scope, $http, Clotho) {
-
-	//todo - add raw versions
+$clotho.extensions.controller('constructionFiles_wetLabSyntaxCtrl', function ($scope, $http) {
 
 });
