7ce0c6402145fc2777170559de18c142f0bb5017
J. Christopher Anderson <jcanderson2167@gmail.com>
2014-07-10 08:23:40 -0700

    unpacking json from python calls
    This can be refactored to be simpler, but it gets the job done.  I
    extract the String from the wrapping object, convert it to Map, then put
    it back in.

--- src/main/java/org/clothocad/core/execution/subprocess/JSONUtil.java
+++ src/main/java/org/clothocad/core/execution/subprocess/JSONUtil.java
@@ -5,14 +5,40 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.clothocad.core.util.JSON;
 
 class JSONUtil {
     private static final ObjectMapper mapper = new ObjectMapper();
 
+    //clotho.get("org.andersonlab.py_fetchRegistry");
+    //clotho.run("org.andersonlab.py_fetchRegistry", ["BBa_b0015"]);
     static Object decodeUTF8(final byte[] bytes) {
         try {
-            String jsonstr = new String(bytes, UTF_8);
-            return mapper.readValue(jsonstr, Object.class);
+            String msg = new String(bytes, UTF_8);
+            LinkedHashMap out = (LinkedHashMap) mapper.readValue(msg, Object.class);
+            //If the out is a wrapped JSON string, then unwrap it
+            try {
+                Object ret = out.get("return");
+                List obs = (List) ret;
+                String jsonstr = (String) obs.get(0);
+                
+
+                Object json = JSON.deserializeObjectToMap(jsonstr);
+                System.out.println(json);
+                
+                //Repackage
+                obs.remove(0);
+                obs.add(0, json);
+                out.put("return", obs);
+                
+                
+            } catch(Exception err) {
+                
+            }
+            return out;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
