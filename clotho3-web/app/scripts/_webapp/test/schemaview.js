c7356bf96534491cf56ac5b01d0292deb22d1b5f
Maxwell Bates <maxbates@gmail.com>
2014-07-24 16:50:19 -0700

    update schemaView demos

--- clotho3-web/app/scripts/_webapp/test/schemaview.js
+++ clotho3-web/app/scripts/_webapp/test/schemaview.js
@@ -3,7 +3,7 @@
 angular.module('clotho.webapp')
   .controller('TestSchemaviewCtrl', function ($scope, Clotho, $clothoModal) {
 
-		Clotho.query({name : "maxbates"}).then(function(results) {
+		Clotho.query({name : "Maxwell Bates"}).then(function(results) {
 			$scope.retrievedId = results.length ? results[0].id : '';
 		});
 
