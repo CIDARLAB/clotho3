fc98a7b2801f9e4312b51c9cb727b822bb2dde1f
Maxwell Bates <maxbates@gmail.com>
2014-08-11 17:06:52 -0700

    fix construction trail page
    close #435

--- clotho3-web/app/scripts/_webapp/test/construction.js
+++ clotho3-web/app/scripts/_webapp/test/construction.js
@@ -3,7 +3,7 @@
 angular.module('clotho.webapp')
   .controller('TestConstructionCtrl', function ($scope, $http, ConstructionSimulator) {
 
-		var files = ['pHA581', 'pSB1A2', 'basic', 'gfp', 'vio'];
+		var files = ['pHA581', 'pSB1A2-Bca9128', 'basic', 'gfp', 'vio'];
 
 	  $http.get('models/construction/construction_'+files[4]+'.json')
 		 .success(function(data) {
@@ -15,6 +15,5 @@ angular.module('clotho.webapp')
 		.success(function(data) {
 			$scope.demoWetLab = data;
 		});
-
 
 	});
