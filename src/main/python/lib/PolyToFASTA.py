06f3f914453c8c514538993f7217929ffbdbbfbe
Mina Li <li.mina888@berkeley.edu>
2014-08-12 11:02:39 -0700

    Added function to turn polynucleotide into FASTA string.

---
+++ src/main/python/lib/PolyToFASTA.py
@@ -0,0 +1,16 @@
+from StringIO import StringIO
+from ClothoPy.convertpoly import _convert_polynucleotide_to_genbank
+import ClothoPy.ClothoSeqIO
+
+def _PolyToFASTA(poly):
+	gen_string = _convert_polynucleotide_to_genbank(poly)
+	gen_handle = StringIO(gen_string)
+	gen_record = ClothoPy.ClothoSeqIO.read(gen_handle, 'gb')
+	out_handle = StringIO()
+	ClothoPy.ClothoSeqIO.write(gen_record, out_handle, "fasta")
+	fasta_data = out_handle.getvalue()
+	return fasta_data
+
+
+def run(*polys):
+    return map(_PolyToFASTA, polys)
