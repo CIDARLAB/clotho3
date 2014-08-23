88e196a589aa61f661a8e84970bca311904ec938
Maxwell Bates <maxbates@gmail.com>
2014-08-22 19:52:18 -0700

    add facebook login page at /import/facebook

---
+++ clotho3-web/app/scripts/_interface/social/facebook.js
@@ -0,0 +1,198 @@
+angular.module('clotho.interface')
+.service('Facebook', function ($q, $window, $filter) {
+
+	var self = this;
+
+	//future - make provider and allow this to be configurable
+	var appId = 1456992994565119;
+
+	var loggingIn = false, 
+			loggedIn = false;
+		
+	var sdkReadyDeferred = $q.defer(),
+			sdkReady = sdkReadyDeferred.promise;
+
+	var user,
+			userReadyDeferred,
+			userReady;
+
+	function setupEmptyUser () {
+		user = {};
+		userReadyDeferred = $q.defer();
+		userReady = userReadyDeferred.promise;
+	}
+
+	//init
+	setupEmptyUser();
+
+	function fetchMe () {
+		loggingIn = true;
+		var deferred = $q.defer();
+
+		sdkReady.then(function () {
+			FB.api('/me', function (response) {
+				handleRetrieveUser(response);
+				deferred.resolve(response);
+			});
+		});
+
+		return deferred.promise;
+	}
+
+	//called on FB.login() or if already logged in
+	function handleRetrieveUser (response) {
+		loggingIn = true;
+		loggedIn = false;
+
+		angular.extend(user, response, {
+			provider : 'facebook',
+			icon : '//graph.facebook.com/' + response.id + '/picture?width=500&height=500'
+		});
+
+		userReadyDeferred.resolve(user);
+	}
+
+	function handleLoginError (response) {
+		loggingIn = false;
+		loggedIn = false;
+
+		userReadyDeferred.reject(response);
+	}
+
+	//todo - handle logout in session
+
+	//callback for JS API init
+	$window.fbAsyncInit = function() {
+		FB.init({
+			appId      : appId,
+			xfbml      : true,
+			version    : 'v2.0',
+			cookie     : true,
+			status     : true
+		});
+
+		FB.getLoginStatus(function(statusReponse) {
+			//if we want the session id, token or anything... thats in here
+			//console.log(statusReponse.authResponse);
+
+			if (statusReponse.status === 'connected') {
+				fetchMe();
+			} else if (statusReponse.status === 'not_authorized') {
+				// The person is logged into Facebook, but not your app.
+			} else {
+				// The person is not logged into Facebook
+			}
+
+			//put this in the callback so we don't call until doing the first check
+			sdkReadyDeferred.resolve(FB);
+		});
+};
+
+	//note - calling this without a button click will probably hide the popup
+	self.login = function (options) {
+
+		var loginOpts = angular.extend({
+			scope: 'public_profile email'
+		}, options);
+
+		var loginDeferred = $q.defer();
+
+		sdkReady.then(function (FB) {
+			if (loggingIn) {
+				userReady.then(function (user) {
+					loginDeferred.resolve(user);
+				});
+			} else {
+				FB.login(function (response) {
+					console.log('FB login response', response);
+					if (response.authResponse) {
+						fetchMe().then(function () {
+							userReady.then(function(userInfo) {
+								loginDeferred.resolve(userInfo);
+							});
+						});
+					} else {
+						loginDeferred.reject(response);
+					}
+				}, loginOpts);
+			}
+		});
+
+		return loginDeferred.promise;
+	};
+
+	self.logout = function (cb) {
+		return sdkReady.then(function (FB) {
+
+			FB.getLoginStatus(function (statusReponse) {
+				if (statusReponse.status === 'connected') {
+					FB.logout(cb)
+				}
+			});
+
+			//note - likely need to reload the page to clear the cookie etc.
+
+			setupEmptyUser();
+		});
+	};
+
+	/* The big daddy. Call this to get things started:
+
+	Facebook.getUser()
+	.then(function (user) {
+		//they are logged in and have authorized app before
+	}, function (err) {
+		//you will want to log them in
+	});
+
+	It is recommended you use both callbacks
+
+	*/
+	self.getUser = function () {
+		var deferred = $q.defer();
+		if (angular.isEmpty(user)) {
+			sdkReady.then(function () {
+				if (loggingIn) {
+					userReady.then(function (user) {
+						deferred.resolve(user);
+					});
+				} else {
+					deferred.reject();
+				}
+			});
+		} else {
+			deferred.resolve(user);
+		}
+		return deferred.promise;
+	};
+
+
+	self.convertToPersonSharable = function (input) {
+		input = input || user;
+		if (angular.isEmpty(user)) {
+			return;
+		}
+
+		return {
+			schema: 'org.clothocad.model.Person',
+			id : input.email,
+			name : input.name,
+			emailAddress : input.email,
+			dateCreated : $filter('date')('yyyy-MM-dd'),
+			icon : input.icon,
+			social : {
+				facebook : input.link
+			}
+		};
+	};
+
+	//IIFE after callbacks etc. set up, will only run when service is instantiated
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "//connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+});
--- clotho3-web/app/scripts/_webapp/import/facebook.js
+++ clotho3-web/app/scripts/_webapp/import/facebook.js
@@ -1,60 +1,55 @@
 'use strict';
 
 angular.module('clotho.webapp')
-.controller('FacebookImportCtrl', function ($scope, Clotho, $window) {
+.controller('FacebookImportCtrl', function ($scope, Clotho, Facebook) {
 
-		$window.fbAsyncInit = function() {
-			FB.init({
-				appId      : '1456992994565119',
-				xfbml      : true,
-				version    : 'v2.0',
-				cookie     : true,
-				status     : true
-			});
+	Facebook.getUser().then(function (user) {
+		//got the user!
+		console.log('got user');
+		$scope.user = user;
+	}, function () {
+		//need to login
+		console.log('need to login');
+		$scope.showLogin = true;
+	});
 
-			FB.getLoginStatus(function(statusReponse) {
-				console.log('statusChangeCallback');
-				console.log(statusReponse);
-				// The response object is returned with a status field that lets the
-				// app know the current login status of the person.
-				// Full docs on the response object can be found in the documentation
-				// for FB.getLoginStatus().
-				if (statusReponse.status === 'connected') {
-					// Logged into your app and Facebook.
-					console.log('Welcome!  Fetching your information.... ');
-					FB.api('/me', function(response) {
-						console.log('Successful login: ', response);
-						$scope.response = 'welcome ' + response.name;
-					});
-				} else if (statusReponse.status === 'not_authorized') {
-					// The person is logged into Facebook, but not your app.
-					$scope.response = 'You are logged into facebook, not this app';
-				} else {
-					// The person is not logged into Facebook, so we're not sure if
-					// they are logged into this app or not.
-					$scope.response = 'Not logged into facebook';
-				}
-			});
+	$scope.login = function () {
+		Facebook.login().then(function (user) {
+			$scope.user = user;
+		}, function (err) {
+			console.log('something didn\'t work');
+			//todo - we will need to re-request if denied: see the API
+		});
+	};
 
-			$scope.login = function (cb) {
-				FB.login(function (response) {
-					console.log('login response', response);
-					cb(response);
-				}, {scope: 'public_profile email'});
-			};
+	$scope.logout = function () {
+		Facebook.logout().then(function () {
+			$scope.user = null;
+			$scope.retrieved = null;
+			$scope.createdId = null;
+			$scope.showLogin = true;
+		});
+	};
 
-			//log out of facebook entirely
-			$scope.logout = FB.logout;
-		};
+	$scope.$watch('user.email', function (newval) {
+		newval && Clotho.get(newval, {mute : true}).then(function (retrieved) {
+			$scope.retrieved = retrieved;
+			$scope.createdId = retrieved.id;
+		});
+	});
 
+	$scope.createUser = function () {
+		if ($scope.user.email) {
+			var person = Facebook.convertToPersonSharable($scope.user);
 
-
-		(function(d, s, id){
-			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {return;}
-			js = d.createElement(s); js.id = id;
-			js.src = "//connect.facebook.net/en_US/sdk.js";
-			fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
+			if (!$scope.retrieved) {
+				Clotho.create(person).then(function (id) {
+					$scope.createdId = id;
+				}, function () {
+					//already existed
+				});
+			}
+		}
+	}
 
 });
