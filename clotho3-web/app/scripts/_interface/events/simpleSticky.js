d4a8b19bf81c994768a0a32a18361fc85b465be6
Maxwell Bates <maxbates@gmail.com>
2014-09-03 18:37:07 -0700

    rewrite simple sticky

--- clotho3-web/app/scripts/_interface/events/simpleSticky.js
+++ clotho3-web/app/scripts/_interface/events/simpleSticky.js
@@ -5,7 +5,9 @@
  * @description
  * Simple directive to create a sticky div, which will remain fixed relative to the top of the page.
  *
- * Not super efficient (is throttled, but still) so don't use too many on a page
+ * Each time the directive is used, the callbacks will be folded into a throttled pipeline (50ms)
+ *
+ * note - requires lodash throttle() and remove()
  *
  * @attr simpleSticky {number} number of pixels from top
  *
@@ -18,59 +20,79 @@ angular.module('clotho.interface')
 
 	var windowEl = angular.element($window);
 
+  var checks = [];
+
+  function getYOffset() {
+    return (angular.isDefined($window.pageYOffset) ?
+            $window.pageYOffset :
+            $window.document[0].documentElement.scrollTop);
+  }
+
+  //the function will be run
+  function addCheck (fn, $scope) {
+    checks.push(fn);
+    $scope.$on('$destroy', removeCheck);
+  }
+
+  function removeCheck(fn) {
+    _.remove(checks, fn);
+  }
+
+  var throttleRunChecks = _.throttle(function () {
+    var pageYOffset = getYOffset();
+
+    angular.forEach(checks, function (fn) {
+      fn.apply(null, [pageYOffset]);
+    });
+  }, 50);
+
+  windowEl.on('scroll resize', throttleRunChecks);
+
 	return {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
+
 			//70 because of sticky nav at top
 			var showFromTopSticky = parseInt(attrs.simpleSticky, 10) || 70,
+        positionNormal = element.css('position'),
 				showFromTopNormal = element.css('top'),
-				startFromTop;
+				startFromTop = element[0].getBoundingClientRect().top + getYOffset(),
+        isAffixed;
 
-			function setOffset () {
-				if (element.hasClass('affix')) {
-					//do nothing
-				} else {
-					var boundingClientRect = element[0].getBoundingClientRect();
-					startFromTop = boundingClientRect.top + (angular.isDefined($window.pageYOffset) ? $window.pageYOffset : $window.document[0].documentElement.scrollTop);
-				}
+      //check if affix state has changed
+			function checkPosition (pageYOffset) {
+				var shouldAffix = (pageYOffset + showFromTopSticky) > startFromTop;
+
+				if (shouldAffix !== isAffixed) {
+          isAffixed = shouldAffix;
+          handleAffixing(shouldAffix);
+        }
 			}
 
-			//init
-			setOffset();
+      //handle class changes, CSS changes
+      function handleAffixing (shouldAffix) {
+        if (shouldAffix) {
+          //don't worry - we are't triggering paint storms because these only run when cross threshold (transform isn't really appropriate)
+          element.css({
+            top: showFromTopSticky + 'px',
+            width: "inherit",
+            position: "fixed"
+          });
+        } else {
+          element.css({
+            top: showFromTopNormal,
+            position: positionNormal
+          });
+        }
+        //element.toggleClass('affix', shouldAffix)
+      }
 
-			function checkPosition () {
-				var affixed = ($window.pageYOffset + showFromTopSticky) > startFromTop;
+      addCheck(function (pageYOffset) {
+        checkPosition(pageYOffset);
+      }, scope);
 
-				if (affixed) {
-					element.css({
-						top: showFromTopSticky + 'px',
-						width: "inherit"
-					});
-				} else {
-					element.css({
-						top: showFromTopNormal
-					});
-				}
-				element.toggleClass('affix', affixed)
-			}
-
-
-			var checkPositionDebounced = _.throttle(function () {
-				checkPosition();
-				setOffset();
-			}, 50);
-
-			//bind listeners
-
-			windowEl.on('scroll', checkPositionDebounced);
-			windowEl.on('resize', checkPositionDebounced);
-
-			//remove on destroy
-
-			scope.$on('$destroy', function () {
-				windowEl.off('scroll', checkPositionDebounced);
-				windowEl.off('resize', checkPositionDebounced);
-			});
+      //init
+      checkPosition();
 		}
-	}
+	};
 });
