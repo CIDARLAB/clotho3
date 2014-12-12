070e44c6523a89347204e884b427fa182b62381a
Stephanie Paige <stmpaige@gmail.com>
2014-12-11 19:55:45 -0500

    Set Shiro CacheManager to MemoryConstrainedCacheManager
    Should stop log warnings about no security cache
    Also, fewer db hits, not that that was an issue

--- src/main/java/org/clothocad/core/security/SecurityModule.java
+++ src/main/java/org/clothocad/core/security/SecurityModule.java
@@ -6,6 +6,8 @@ package org.clothocad.core.security;
 
 import com.google.inject.binder.AnnotatedBindingBuilder;
 import com.google.inject.name.Names;
+import org.apache.shiro.cache.CacheManager;
+import org.apache.shiro.cache.MemoryConstrainedCacheManager;
 import org.apache.shiro.guice.web.ShiroWebModule;
 import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -35,10 +37,13 @@ public class SecurityModule extends ShiroWebModule {
             bind(ServletContextHandler.class).annotatedWith(Names.named("containerServletContext")).toInstance(handler);
             expose(ServletContextHandler.class).annotatedWith(Names.named("containerServletContext"));
             ShiroWebModule.bindGuiceFilter(binder());
+            bind(CacheManager.class).to(MemoryConstrainedCacheManager.class);
     }
 
     @Override
     protected void bindSessionManager(AnnotatedBindingBuilder<SessionManager> bind) {
         bind.to(DefaultWebSessionManager.class);
     }
+    
+    
 }
