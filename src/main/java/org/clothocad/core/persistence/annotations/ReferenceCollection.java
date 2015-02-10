8b7eb25bef1efc2f48954beec0b490c6870f631d
Bill Cao <billcao@berkeley.edu>
2015-02-10 13:40:59 -0800

    ReferenceCollection in proper directory

--- src/main/java/org/clothocad/core/persistence/annotations/ReferenceCollection.java
+++ src/main/java/org/clothocad/core/persistence/annotations/ReferenceCollection.java
@@ -1,18 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.clothocad.core.persistence.annotations;
+
+import org.clothocad.core.persistence.jackson.ReferenceDeserializer;
+import org.clothocad.core.persistence.jackson.ReferenceSerializer;
 
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import org.clothocad.core.persistence.jackson.ReferenceDeserializer;
-import org.clothocad.core.persistence.jackson.ReferenceSerializer;
 
 /**
  * Annotation flag marking a property that should be serialized/deserialized 
