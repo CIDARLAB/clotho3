ae1757ce19d737ac29285995c36731b79deb144a
Bill Cao <billcao@berkeley.edu>
2015-02-19 02:05:39 -0800

    Commented out unused code disabled unused loading of all functions in function.js

--- clotho3-web/app/scripts/_editor/controllers/function.js
+++ clotho3-web/app/scripts/_editor/controllers/function.js
@@ -1,173 +1,173 @@
 angular.module('clotho.editor')
 .controller('Editor_FunctionCtrl', function($scope, Clotho, $filter, $q, codemirrorLoader, ClothoSchemas, $timeout) {
 
-	/* data types */
+    /* data types */
 
-	$scope.langTypes = [
-		{name:'JavaScript', value:'JAVASCRIPT'},
-		{name:'Java', value:'JAVA'},
-		{name:'Python', value:'PYTHON'},
-		{name:'Groovy', value:'GROOVY'}
-	];
+    $scope.langTypes = [
+        {name:'JavaScript', value:'JAVASCRIPT'},
+        {name:'Java', value:'JAVA'},
+        {name:'Python', value:'PYTHON'},
+        {name:'Groovy', value:'GROOVY'}
+    ];
 
-	$scope.outputTypes = [
-		{name:'Value', value:'VALUE'},
-		{name:'Reference', value:'REFERENCE'}
-	];
+    $scope.outputTypes = [
+        {name:'Value', value:'VALUE'},
+        {name:'Reference', value:'REFERENCE'}
+    ];
 
-	$scope.simpleTypes = {
-		"object" : true,
-		"string" : true,
-		"number" : true,
-		"boolean" : true,
-		"array" : true
-	};
+    $scope.simpleTypes = {
+        "object" : true,
+        "string" : true,
+        "number" : true,
+        "boolean" : true,
+        "array" : true
+    };
 
-	$scope.paramTypes = [];
+    $scope.paramTypes = [];
 
-	angular.forEach(ClothoSchemas.primitiveToJava, function (val, key) {
-		$scope.paramTypes.push({
-			id : key,
-			name : key,
-			type : key,
-			category : 'Primitive',
-			reference : false
-		});
-	});
+    angular.forEach(ClothoSchemas.primitiveToJava, function (val, key) {
+        $scope.paramTypes.push({
+            id : key,
+            name : key,
+            type : key,
+            category : 'Primitive',
+            reference : false
+        });
+    });
 
-	ClothoSchemas.retrievedSchemas.then(function (schemas) {
-		angular.forEach(schemas, function(schema){
-			$scope.paramTypes.push(angular.extend(schema, {category:'Schema'}));
-		});
-	});
+    ClothoSchemas.retrievedSchemas.then(function (schemas) {
+        angular.forEach(schemas, function(schema) {
+            $scope.paramTypes.push(angular.extend(schema, {category:'Schema'}));
+        });
+    });
 
-	// todo - this will likely not be practical after release
-	$scope.clothoFunctions = [];
-	Clotho.query({schema : ClothoSchemas.sharableTypes.Function.schema}, {mute : true}).then(function(result) {
-		$scope.clothoFunctions = result;
-	});
+    // todo - this will likely not be practical after release
+    // note - currently unused as methods section in schema.html commented out
+    // $scope.clothoFunctions = [];
+    // Clotho.query({schema : ClothoSchemas.sharableTypes.Function.schema}, {mute : true}).then(function(result) {
+    //     $scope.clothoFunctions = result;
+    // });
 
-	$scope.querySchemaWrapper = function(schemaType, value) {
-		return Clotho.autocomplete(value).then(function (results) {
-			return _.filter(results, function (result) {
-				return result.schema == schemaType;
-			});
-		});
-	};
+    $scope.querySchemaWrapper = function(schemaType, value) {
+        return Clotho.autocomplete(value).then(function (results) {
+            return _.filter(results, function (result) {
+                return result.schema == schemaType;
+            });
+        });
+    };
 
-	/* args + deps */
+    /* args + deps */
 
-	$scope.addArg = function() {
-		if (angular.isEmpty($scope.sharable.args)) {
-			$scope.sharable.args = [];
-		}
-		$scope.sharable.args.push({"type" : "", "name" : ""});
-	};
+    $scope.addArg = function() {
+        if (angular.isEmpty($scope.sharable.args)) {
+            $scope.sharable.args = [];
+        }
+        $scope.sharable.args.push({"type" : "", "name" : ""});
+    };
 
-	$scope.addDep = function() {
-		if (angular.isEmpty($scope.sharable.dependencies)) {
-			$scope.sharable.dependencies = [];
-		}
-		if ($scope.newDependencyModel != '') {
-			$scope.sharable.dependencies.push($scope.newDependencyModel);
-		}
-		$scope.newDependencyModel = '';
-	};
+    $scope.addDep = function() {
+        if (angular.isEmpty($scope.sharable.dependencies)) {
+            $scope.sharable.dependencies = [];
+        }
+        if ($scope.newDependencyModel != '') {
+            $scope.sharable.dependencies.push($scope.newDependencyModel);
+        }
+        $scope.newDependencyModel = '';
+    };
 
-	/* tests */
+    /* tests */
 
-	$scope.addTest = function() {
-		if (angular.isEmpty($scope.sharable.tests)) {$scope.sharable.tests = [];}
-		$scope.sharable.tests.push({"args" : [], "output" : {"value" : "", "type" : ""}});
-	};
+    $scope.addTest = function() {
+        if (angular.isEmpty($scope.sharable.tests)) {$scope.sharable.tests = [];}
+        $scope.sharable.tests.push({"args" : [], "output" : {"value" : "", "type" : ""}});
+    };
 
-	$scope.testResults = {};
-	$scope.singleTest = function(index) {
+    $scope.testResults = {};
+    $scope.singleTest = function(index) {
 
-		var data = {};
-		data.id = $scope.sharable.id;
-		if (angular.isEmpty($scope.sharable.tests)) {
-			data.args = [];
-		}
-		else {
-			data.args = $scope.sharable.tests[index].args;
-		}
+        var data = {};
+        data.id = $scope.sharable.id;
+        if (angular.isEmpty($scope.sharable.tests)) {
+            data.args = [];
+        } else {
+            data.args = $scope.sharable.tests[index].args;
+        }
 
-		//fixme - need to resolve arguments which are Clotho objects (they should be IDs)
-		//future - will need to do the same for parameterized arguments
+        //fixme - need to resolve arguments which are Clotho objects (they should be IDs)
+        //future - will need to do the same for parameterized arguments
 
-		var resolvedArgs = [];
-		_.forEach(data.args, function (arg, index) {
-			if ($scope.simpleTypes[$scope.sharable.args[index].type]) {
-				resolvedArgs.push(arg);
-			} else {
-				resolvedArgs.push(Clotho.get(arg, {mute : true}).then(function (r) { return r}))
-			}
-		});
+        var resolvedArgs = [];
+        _.forEach(data.args, function (arg, index) {
+            if ($scope.simpleTypes[$scope.sharable.args[index].type]) {
+                resolvedArgs.push(arg);
+            } else {
+                resolvedArgs.push(Clotho.get(arg, {mute : true}).then(function (r) { return r}))
+            }
+        });
 
-		$q.all(resolvedArgs).then(function (resolved) {
-			Clotho.run(data.id, resolved)
-			.then(function onFunctionTestSuccess (result){
-				$scope.testResults[index] = angular.equals(result, $scope.sharable.tests[index].output.value);
-			}, function onFunctionTestError () {
-				Clotho.alert('test errored! See activity log.');
-			});
-		});
-
-
-	};
-
-	$scope.runAllTests = function() {
-		for (var i = 0; i < $scope.sharable.tests.length; i++) {
-			$scope.singleTest(i);
-		}
-	};
-
-	$scope.resetTests = function() {
-		$scope.testResults = {};
-	};
-
-	//overwrite save to reset tests and set code pristine
-	$scope.save = function() {
-		$scope.resetTests();
-		$scope.Code.$setPristine();
-		$scope.$parent.save()
-	};
+        $q.all(resolvedArgs).then(function (resolved) {
+            Clotho.run(data.id, resolved)
+            .then(function onFunctionTestSuccess (result){
+                $scope.testResults[index] = angular.equals(result, $scope.sharable.tests[index].output.value);
+            }, function onFunctionTestError () {
+                Clotho.alert('test errored! See activity log.');
+            });
+        });
 
 
-	// code mirror
+    };
 
-	$scope.$watch('editMode', function(newval) {
-		$scope.codemirrorEditorOptions.readOnly = (newval) ? false : 'nocursor';
-		$scope.resetTests();
-	});
+    $scope.runAllTests = function() {
+        for (var i = 0; i < $scope.sharable.tests.length; i++) {
+            $scope.singleTest(i);
+        }
+    };
 
-	$scope.codemirrorEditorOptions = {
-		lineWrapping : true,
-		lineNumbers: true,
-		// HACK to have the codemirror instance in the scope...
-		onLoad : function(_cm){
-			$scope.$watch('sharable.language', function(newlang, oldlang) {
-				if (!!newlang) {
-					var mode = newlang.toLowerCase();
+    $scope.resetTests = function() {
+        $scope.testResults = {};
+    };
 
-					codemirrorLoader.loadLanguage(mode).then(function () {
-						// HACK to catch java case
-						mode = (mode == 'java') ? 'text/x-java' : mode;
-						//give it time to register
-						$timeout(function () {
-							_cm.setOption("mode", mode);
-						}, 500);
-					});
-				}
-			});
+    //overwrite save to reset tests and set code pristine
+    $scope.save = function() {
+        $scope.resetTests();
+        $scope.Code.$setPristine();
+        $scope.$parent.save()
+    };
 
-			// example Events
-			_cm.on("beforeChange", function(){});
-			_cm.on("change", function(){});
-		}
-	};
 
-	//init()
-	$scope.resetTests();
+    // code mirror
+
+    $scope.$watch('editMode', function(newval) {
+        $scope.codemirrorEditorOptions.readOnly = (newval) ? false : 'nocursor';
+        $scope.resetTests();
+    });
+
+    $scope.codemirrorEditorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        // HACK to have the codemirror instance in the scope...
+        onLoad : function(_cm){
+            $scope.$watch('sharable.language', function(newlang, oldlang) {
+                if (!!newlang) {
+                    var mode = newlang.toLowerCase();
+
+                    codemirrorLoader.loadLanguage(mode).then(function () {
+                        // HACK to catch java case
+                        mode = (mode == 'java') ? 'text/x-java' : mode;
+                        //give it time to register
+                        $timeout(function () {
+                            _cm.setOption("mode", mode);
+                        }, 500);
+                    });
+                }
+            });
+
+            // example Events
+            _cm.on("beforeChange", function(){});
+            _cm.on("change", function(){});
+        }
+    };
+
+    //init()
+    $scope.resetTests();
 });
--- clotho3-web/app/scripts/_editor/controllers/schema.js
+++ clotho3-web/app/scripts/_editor/controllers/schema.js
@@ -1,122 +1,123 @@
 angular.module('clotho.editor').controller('Editor_SchemaCtrl', function($scope, Clotho, ClothoSchemas) {
 
-	$scope.schemas = [];
+    $scope.schemas = [];
 
-	ClothoSchemas.retrievedSchemas.then(function (schemas) {
-		$scope.schemas = schemas;
-	});
+    ClothoSchemas.retrievedSchemas.then(function (schemas) {
+        $scope.schemas = schemas;
+    });
 
-	$scope.clothoFunctionWrap = function (query) {
-		return Clotho.query({"schema": ClothoSchemas.sharableTypes.Function.schema, name : query}, {mute : true})
-		.then(function(data) {
-			return data || [];
-		});
-	};
+    $scope.clothoFunctionWrap = function (query) {
+        return Clotho.query({"schema": ClothoSchemas.sharableTypes.Function.schema, name : query}, {mute : true})
+        .then(function(data) {
+            return data || [];
+        });
+    };
 
-	$scope.accessTypes = ClothoSchemas.accessTypes;
+    $scope.accessTypes = ClothoSchemas.accessTypes;
 
-	$scope.constraintTypes = ClothoSchemas.constraintTypes;
+    $scope.constraintTypes = ClothoSchemas.constraintTypes;
 
-	$scope.primitiveToJava = ClothoSchemas.primitiveToJava;
+    $scope.primitiveToJava = ClothoSchemas.primitiveToJava;
 
-	$scope.findSpacesRegExp = /\s/ig;
+    $scope.findSpacesRegExp = /\s/ig;
 
-	//TODO - dry. duplicates function editor code
-	$scope.paramTypes = [
-		{name:'object', type: "object", category:'Primitive', reference: false},
-		{name:'array', type : "array", category:'Primitive', reference: false},
-		{name:'string', type : "string", category:'Primitive', reference: false},
-		{name:'number', type : "number", category:'Primitive', reference: false},
-		{name:'boolean', type : "boolean", category:'Primitive', reference: false}
-	];
+    //TODO - dry. duplicates function editor code
+    $scope.paramTypes = [
+        {name:'object', type: "object", category:'Primitive', reference: false},
+        {name:'array', type : "array", category:'Primitive', reference: false},
+        {name:'string', type : "string", category:'Primitive', reference: false},
+        {name:'number', type : "number", category:'Primitive', reference: false},
+        {name:'boolean', type : "boolean", category:'Primitive', reference: false}
+    ];
 
-	ClothoSchemas.retrievedSchemas.then(function (schemas) {
-		angular.forEach(schemas, function(schema){
-			$scope.paramTypes.push(angular.extend(schema, {category:'Schema'}));
-		});
-	});
+    ClothoSchemas.retrievedSchemas.then(function (schemas) {
+        angular.forEach(schemas, function(schema){
+            $scope.paramTypes.push(angular.extend(schema, {category:'Schema'}));
+        });
+    });
 
 
-	$scope.$watch('sharable.superClass', function(val) {
-		if (!!val) {
-			$scope.getSuperClass();
-		}
-	});
+    $scope.$watch('sharable.superClass', function(val) {
+        if (!!val) {
+            $scope.getSuperClass();
+        }
+    });
 
-	$scope.getSuperClass = function () {
-		ClothoSchemas.getSuperclassFields($scope.sharable)
-		.then(function(result) {
-			$scope.superClassFields = result;
-			$scope.superClassObj = result;
-		});
-	};
+    $scope.getSuperClass = function () {
+        ClothoSchemas.getSuperclassFields($scope.sharable)
+        .then(function(result) {
+            $scope.superClassFields = result;
+            $scope.superClassObj = result;
+        });
+    };
 
-	$scope.newMethod = function() {
-		return ""
-	};
+    // note - currently unused as methods section in schema.html commented out
+    // $scope.newMethod = function() {
+    //     return ""
+    // };
 
-	$scope.addMethod = function(method) {
-		if (angular.isEmpty($scope.sharable.methods)) {$scope.sharable.methods = [];}
-		$scope.sharable.methods.push(method);
-	};
+    // $scope.addMethod = function(method) {
+    //     if (angular.isEmpty($scope.sharable.methods)) {$scope.sharable.methods = [];}
+    //     $scope.sharable.methods.push(method);
+    // };
 
-	$scope.addNewMethod = function() {
-		if (angular.isEmpty($scope.newMethodObj)) return;
+    // $scope.addNewMethod = function() {
+    //     if (angular.isEmpty($scope.newMethodObj)) return;
 
-		$scope.addMethod($scope.newMethodObj);
-		$scope.newMethodObj = $scope.newMethod();
-	};
+    //     $scope.addMethod($scope.newMethodObj);
+    //     $scope.newMethodObj = $scope.newMethod();
+    // };
 
-	$scope.determineMethodName = function(id) {
-		return _.find($scope.clothoFunctions, {id : id}).name;
-	};
+    // $scope.determineMethodName = function(id) {
+    //  return _.find($scope.clothoFunctions, {id : id}).name;
+    // };
 
-	$scope.newField = function() {
-		return {
-			name: "",
-			type: "",
-			description: "",
-			example: "",
-			constraints: null,
-			access: "PUBLIC"
-		}
-	};
+    $scope.newField = function() {
+        return {
+            name: "",
+            type: "",
+            description: "",
+            example: "",
+            constraints: null,
+            access: "PUBLIC"
+        }
+    };
 
-	$scope.addField = function() {
-		if (angular.isEmpty($scope.sharable.fields)) {$scope.sharable.fields = [];}
-		$scope.sharable.fields.push($scope.newField());
-	};
+    $scope.addField = function() {
+        if (angular.isEmpty($scope.sharable.fields)) {$scope.sharable.fields = [];}
+        $scope.sharable.fields.push($scope.newField());
+    };
 
-	//constraints are processed in saveSchema (link function)
-	$scope.newConstraint = function() {
-		return {
-			type: "",
-			value: ""
-		};
-	};
+    //constraints are processed in saveSchema (link function)
+    $scope.newConstraint = function() {
+        return {
+            type: "",
+            value: ""
+        };
+    };
 
-	$scope.addConstraint = function(index) {
-		if (angular.isEmpty($scope.sharable.fields[index].constraints))
-			$scope.sharable.fields[index].constraints = [];
-		$scope.sharable.fields[index].constraints.push($scope.newConstraint())
-	};
+    $scope.addConstraint = function(index) {
+        if (angular.isEmpty($scope.sharable.fields[index].constraints))
+            $scope.sharable.fields[index].constraints = [];
+        $scope.sharable.fields[index].constraints.push($scope.newConstraint())
+    };
 
-	//overwrite the save function for schemas specifically
-	$scope.save = function () {
-		//process constraints to object
-		angular.forEach($scope.sharable.fields, function(field) {
-			console.log(field);
-			if (field.constraints) {
-				var constraintsArray = field.constraints;
-				field.constraints = {};
-				angular.forEach(constraintsArray, function(constraint) {
-					field.constraints[constraint.type] = constraint.value;
-				});
-			} else {
-				field.constraints = null;
-			}
-		});
-		$scope.$parent.save();
-	};
+    //overwrite the save function for schemas specifically
+    $scope.save = function () {
+        //process constraints to object
+        angular.forEach($scope.sharable.fields, function(field) {
+            console.log(field);
+            if (field.constraints) {
+                var constraintsArray = field.constraints;
+                field.constraints = {};
+                angular.forEach(constraintsArray, function(constraint) {
+                    field.constraints[constraint.type] = constraint.value;
+                });
+            } else {
+                field.constraints = null;
+            }
+        });
+        $scope.$parent.save();
+    };
 
 });
