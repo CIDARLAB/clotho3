6777ad3f82b5ca08df1a981212cf04e62647f688
Mina Li <li.mina888@berkeley.edu>
2015-02-24 13:50:18 -0800

    Some tweaks to improve identification of uniprot vs. ncbi IDs.

--- src/main/python/lib/ids_to_operon.py
+++ src/main/python/lib/ids_to_operon.py
@@ -20,34 +20,46 @@ def process_polypeptide2(poly_list):
 	# pairs = {}
 	for poly in poly_list:
 
-
-		retriever.retrieve_gb([poly]) # get from ncbi
-		# print len(retriever.records)
 		# try UniProt
 		next_check = False
-		if len(retriever.records) == 0:
-			# print "try uni"
+		try:
+			attempt = fetch_uniprot(poly)
 			uni = uniprot_to_ncbi(poly)
 			if poly in uni.keys():
 				ncbi = uni[poly]
 				retriever.retrieve_gb([ncbi])
 				record = retriever.records[0]
 				orf = _protein_to_orf(record)
+				# if orf is None:
+					# print "uni none"
 				orfs.append(orf)
 			else:
 				# print "check me"
 				next_check = True
-		else:
+		except Exception:
 			# print "check me"
 			next_check = True
 
 		# NCBI ids
-		if next_check and len(retriever.records) > 0:
+		if next_check:
+			retriever.retrieve_gb([poly]) # get from ncbi
+			# print len(retriever.records)
 			# print "try ncbi"
-			record = retriever.records[0]
-			orf = _protein_to_orf(record)
-			orfs.append(orf)
-		elif next_check:
+			if len(retriever.records) > 0:
+				record = retriever.records[0]
+				orf = _protein_to_orf(record)
+				if orf is None:
+					# print "ncbi none " + record.id
+					# explicit
+					next_check = True
+				else:
+					orfs.append(orf)
+					next_check = False
+			else:
+				# explicit
+				next_check = True
+
+		if next_check:
 			# print "try blast"
 			seq = fetch_uniprot(poly)['sequence']
 			blaster = call_blast(seq, 'me@example.com')
@@ -65,6 +77,7 @@ Main function.
 """
 def ids_to_operon(query):
 	orf_list = process_polypeptide2(query)
+	# print orf_list
 	rbs_list = [{'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR15', 'name': u'UTR15', 'sequence': u'AATTGTGAGCGGATAACAATTTCACACAGGAAACAGCTATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR12', 'name': u'UTR12', 'sequence': u'aattgtgagcggataacaatttcacacaggaaacaCATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR16', 'name': u'UTR16', 'sequence': u'AATTGTGAGCGGATAACAATTTTAAGGAGATATACATATG'}, {'isIntergenic': True, 'id': u'org.clothocad.model.RBS.interRBS1', 'name': u'interRBS1', 'sequence': u'taaGAGCTCAAGGAAAGAAAAatg'}, {'isIntergenic': True, 'id': u'org.clothocad.model.RBS.interRBS2', 'name': u'interRBS2', 'sequence': u'tagATAACGAGGGCAAAAAatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR1', 'name': u'UTR1', 'sequence': u'ACCCGTTTTTTTGGGCTAACAGGAGGAATTAACCATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR10', 'name': u'UTR10', 'sequence': u'ACCCGTTTTTTTGGGCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR11', 'name': u'UTR11', 'sequence': u'GGGCAGATCTTCGAATGCATCGCGCGCACCGTACGTCTCGAGGAATTCCTGCAGGATATCTGGATCCACGAAGCTTCCCATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR13', 'name': u'UTR13', 'sequence': u'GGGAGACCACAACGGTTTCCCTCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR14', 'name': u'UTR14', 'sequence': u'GGCTAGCCTCGAGAATTCACGCGTGGTACCTCTAGAGTCGACCCGGGCGGCCGCAAAGTTCCCTTTAGTGAGGGTTAATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR17', 'name': u'UTR17', 'sequence': u'GGGCGAATTGGGTACCGGGCCCAGACCACAACGGTTTCCCTCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR18', 'name': u'UTR18', 'sequence': u'ACCCGTTTTTTTGGGCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATACCCATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR19', 'name': u'UTR19', 'sequence': u'acccgtttttttgggctagcaccgcctatctcgtgtgagataggcggagatacgaactttaagaaggagatatacacatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR2', 'name': u'UTR2', 'sequence': u'GGGAGACCACAACGGTTTCCCTCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR20', 'name': u'UTR20', 'sequence': u'AATTGTGAGCGGATAACAATTTCACACAGGAAACAGCCAGTCCGTTTAGGTGTTTTCACGAGCACTTCACCAACAAGGACCATAGcatatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR21', 'name': u'UTR21', 'sequence': u'aattgtgagcggataacaatttcacaCAAGGAGGAAACAGCTatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR22', 'name': u'UTR22', 'sequence': u'aattgtgagcggataacaatttcacacaggaaacagaccatggaattcAGGAGCGACTACatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR23', 'name': u'UTR23', 'sequence': u'aattgtgagcggataacaatttcacacaggaaacagctatgaccatgattacgaattcGAGGAAGTGGTATatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR24', 'name': u'UTR24', 'sequence': u'gggcgaattgggtaccgggccccccctcgaggtcgacggtatcgataagcttgatatcgaattcctgcagcccgggGAGGAGAGAAATTatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR3', 'name': u'UTR3', 'sequence': u'GGGCGAATTCGTTAACTTTAAGAAGGAGATATACCCATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR4', 'name': u'UTR4', 'sequence': u'accCGTTTTTTTGGGCTAACATCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR5', 'name': u'UTR5', 'sequence': u'accCGTTTTTTTGGGCTATTAATTAAGCGGCCGCCCTGCAGGACTCGAGTTCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR6', 'name': u'UTR6', 'sequence': u'ACCCGTTTTTTGGGCTAACAGGAGGAATTAACCATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR7', 'name': u'UTR7', 'sequence': u'acccgtttttttgggctagcaccgcctatctcgtgtgagataggcggagatacgaactttaagaaggagatatacccatg'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR8', 'name': u'UTR8', 'sequence': u'ACCCGTTTTTTGGGCTAGAAATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}, {'isIntergenic': False, 'id': u'org.clothocad.model.RBS.UTR9', 'name': u'UTR9', 'sequence': u'ATAATTTTGTTTAACTTTAAGAAGGAGATATACATATG'}]
 	operon = design_operon(orf_list, rbs_list, 6)
 	return operon
