5aa7cf5fda2e2be6256dd6206660c5ff61a2fe2e
Bill Cao <billcao@berkeley.edu>
2015-02-24 16:16:52 -0800

    LearningClotho trails added manipulating DNA widgets need to be fixed

--- clotho3-web/app/scripts/_webapp/home.js
+++ clotho3-web/app/scripts/_webapp/home.js
@@ -1,28 +1,18 @@
 angular.module('clotho.webapp').controller('HomeCtrl', function ($scope, Clotho, hotkeys) {
-	$scope.modalContent = '<p>Welcome to Clotho!</p>' +
-		'<p>Clotho is a platform for automating your genetic engineering projects. Learn how to use Clotho by starting the trail below!</p>';
+    $scope.modalContent = '<p>Welcome to Clotho!</p>' +
+        '<p>Clotho is a platform for automating your genetic engineering projects. Learn how to use Clotho by starting the trail below!</p>';
 
+    $scope.downloadClotho = function(){
+        window.open("https://github.com/CIDARLAB/clotho3", '_blank');
+    };
 
-  $scope.downloadClotho = function(){
-    window.open("https://github.com/CIDARLAB/clotho3", '_blank');
-  };
-  $scope.browseApps = function(){
-    window.open("https://github.com/CIDARLAB/clotho3apps", '_blank');
-  };
+    $scope.browseApps = function(){
+        window.open("https://github.com/CIDARLAB/clotho3apps", '_blank');
+    };
 
-	$scope.enterClotho = function() {
-
-    window.open("http://synbiotrails.org/#!/trail?id=org.clothocad.trails.LearningClotho", '_blank');
-    //Clotho.startTrail('org.clothocad.trails.LearningClotho');
-	};
-
-	$scope.enterEugene = function() {
-		Clotho.startTrail('org.clothocad.trails.EugeneCADIntro');
-	};
-
-	$scope.enterRaven = function() {
-		Clotho.startTrail('org.clothocad.trails.RavenCADIntro');
-	};
+    $scope.enterClotho = function() {
+        Clotho.startTrail('org.clothocad.trails.LearningClotho');
+    };
 
   //register listener in controller as callback instead of expression so doesn't get triggered in inputs
   hotkeys.bindTo($scope)
