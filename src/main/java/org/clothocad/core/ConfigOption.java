b807bc1c8fd6be2f73e556d4bbdcc7b1d1f3f06a
Bill Cao <billcao@berkeley.edu>
2015-03-09 23:23:29 -0700

    org.clothocad.core style cleanup

--- src/main/java/org/clothocad/core/ConfigOption.java
+++ src/main/java/org/clothocad/core/ConfigOption.java
@@ -6,35 +6,32 @@ import org.apache.commons.cli.Options;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-/**
- *
- * @author spaige
- */
 public enum ConfigOption {
     port("HTTP listening port", "8080", "port"),
     confidentialport("HTTPS listening port", "8443", "port"),
     dbname("database name", "clotho", "name"),
     dbhost("database url hostname", "localhost", "hostname"),
     dbport("database url port", "27017", "port"),
-    //loglevel,
+    // loglevel,
     keystorepath("SSL keystore path",
-                 Paths.get(System.getProperty("java.home"))
-                      .resolve(Paths.get("lib", "security", "cacerts"))
-                      .toString(),
-                 "path"),
+                Paths.get(System.getProperty("java.home"))
+                   .resolve(Paths.get("lib", "security", "cacerts"))
+                   .toString(),
+                "path"),
     keystorepass("SSL keystore password", "", "password"),
     configfile("path to configuration file",
-               Paths.get(System.getProperty("user.home"))
+                Paths.get(System.getProperty("user.home"))
                     .resolve(Paths.get(".clothoconfig"))
                     .toString(),
-               "path"),
+                "path"),
     clientdirectory("path to client files directory",
-                    Paths.get("clotho3-web", "dist").toString(),
-                    "path");
-    
+                Paths.get("clotho3-web", "dist").toString(),
+                "path");
+
     final String description;
     final String defaultValue;
     final String argName;
+
     private ConfigOption(final String description,
                          final String defaultValue,
                          final String argName) {
@@ -43,10 +40,8 @@ public enum ConfigOption {
         this.argName = argName;
     }
 
-    private Option
-    toOption() {
-        final String desc =
-            String.format("%s (default: %s)", description, defaultValue);
+    private Option toOption() {
+        final String desc = String.format("%s (default: %s)", description, defaultValue);
         final Option out = new Option(this.name(), true, desc);
         out.setArgName(argName);
         return out;
@@ -54,8 +49,9 @@ public enum ConfigOption {
 
     public static Options getOptions() {
         Options options = new Options();
-        for (ConfigOption opt : ConfigOption.values())
+        for (ConfigOption opt : ConfigOption.values()) {
             options.addOption(opt.toOption());
+        }
         options.addOption(new Option("help", false, "print this message"));
         return options;
     }
@@ -63,8 +59,9 @@ public enum ConfigOption {
     /** TODO: the only legitimate user is org.clothocad.core.util.Config.get */
     public static Properties getDefaultConfig() {
         final Properties out = new Properties();
-        for (final ConfigOption c : ConfigOption.values())
+        for (final ConfigOption c : ConfigOption.values()) {
             out.setProperty(c.name(), c.defaultValue);
+        }
         return out;
     }
 }
