a09866bac104f5a9c8d55b2752ac7cf79663c659
PrashantVaidyanathan <prash@bu.edu>
2015-07-01 19:28:39 -0400

    added creative commons and Trademark sentence

--- src/main/java/org/clothocad/model/Feature.java
+++ src/main/java/org/clothocad/model/Feature.java
@@ -1,24 +1,24 @@
 /*
-Copyright (c) 2009 The Regents of the University of California.
-All rights reserved.
-Permission is hereby granted, without written agreement and without
-license or royalty fees, to use, copy, modify, and distribute this
-software and its documentation for any purpose, provided that the above
-copyright notice and the following two paragraphs appear in all copies
-of this software.
+ Copyright (c) 2009 The Regents of the University of California.
+ All rights reserved.
+ Permission is hereby granted, without written agreement and without
+ license or royalty fees, to use, copy, modify, and distribute this
+ software and its documentation for any purpose, provided that the above
+ copyright notice and the following two paragraphs appear in all copies
+ of this software.
 
-IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
-FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
-ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
-THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
-SUCH DAMAGE.
+ IN NO EVENT SHALL THE UNIVERSITY OF CALIFORNIA BE LIABLE TO ANY PARTY
+ FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ THE UNIVERSITY OF CALIFORNIA HAS BEEN ADVISED OF THE POSSIBILITY OF
+ SUCH DAMAGE.
 
-THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
-PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
-CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
-ENHANCEMENTS, OR MODIFICATIONS..
+ THE UNIVERSITY OF CALIFORNIA SPECIFICALLY DISCLAIMS ANY WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE SOFTWARE
+ PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, AND THE UNIVERSITY OF
+ CALIFORNIA HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS..
  */
 package org.clothocad.model;
 
@@ -39,31 +39,32 @@ import org.clothocad.core.datums.SharableObjBase;
  */
 @NoArgsConstructor
 public class Feature extends SharableObjBase {
-	
-	@Setter
+
+    @Setter
     @Getter
     @Reference
-    protected Sequence sequence;
-    
+    private Sequence sequence;
+
     @Setter
     @Getter
     protected String genbankId, swissProtId, PDBId;
-    
+
     @Getter
     protected short riskGroup;
-   
+
     @NotNull
     @Setter
     @Getter
     protected FeatureRole role;
-    
+
     @Getter
-	@Setter
-	@Reference
-	protected Feature parentFeature;
-    
+    @Setter
+    @Reference
+    protected Feature parentFeature;
+
     /**
      * Constructor of a new Feature
+     *
      * @param name
      * @param role
      * @param author
@@ -72,9 +73,10 @@ public class Feature extends SharableObjBase {
         super(name, author);
         this.role = role;
     }
-    
+
     /**
      * Constructor of a new Feature
+     *
      * @param name
      * @param description
      * @param role
@@ -86,21 +88,23 @@ public class Feature extends SharableObjBase {
     }
 
     /**
-     * Change the risk group of the Feature.  You can only raise the risk group.
+     * Change the risk group of the Feature. You can only raise the risk group.
+     *
      * @param newrg the new risk group (1 through 5)
      */
     public final void setRiskGroup(short newrg) {
         if (newrg > riskGroup && newrg <= 5) {
             //addUndo("_riskGroup", _featDatum._riskGroup, newrg);
             riskGroup = newrg;
-           // setChanged(org.clothocore.api.dnd.RefreshEvent.Condition.RISK_GROUP_CHANGED);
+            // setChanged(org.clothocore.api.dnd.RefreshEvent.Condition.RISK_GROUP_CHANGED);
         }
         //todo: throw appropriate invalid operation exception
     }
-    
+
     // Feel free to add more of these
     public static enum FeatureRole {
-    	PROMOTER, CDS, RBS, TERMINATOR, SCAR, SPACER, RIBOZYME;
+
+        PROMOTER, CDS, RBS, TERMINATOR, SCAR, SPACER, RIBOZYME;
     }
 
 }
