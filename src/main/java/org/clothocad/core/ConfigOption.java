8314ba147854be7d890a9274b1f8b75c436c2778
PrashantVaidyanathan <prash@bu.edu>
2016-07-04 15:48:55 -0400

    changed default webapp back to dist

--- src/main/java/org/clothocad/core/ConfigOption.java
+++ src/main/java/org/clothocad/core/ConfigOption.java
@@ -25,7 +25,7 @@ public enum ConfigOption {
                     .toString(),
                 "path"),
     clientdirectory("path to client files directory",
-                Paths.get("clotho3-web", "dist-trails").toString(),
+                Paths.get("clotho3-web", "dist").toString(),
                 "path");
 
     final String description;
