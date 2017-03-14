561f80eac61eac3288b36c0a2775ca91aa2cd261
alexvahid <avahid@bu.edu>
2017-03-14 15:48:57 -0400

    REST Tester Works

--- src/test/java/org/clothocad/core/util/RESTTester.java
+++ src/test/java/org/clothocad/core/util/RESTTester.java
@@ -8,14 +8,27 @@ package org.clothocad.core.util;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.Socket;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.apache.http.client.HttpClient;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.ConnectTimeoutException;
@@ -26,6 +39,7 @@ import org.apache.http.conn.scheme.SchemeSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.SingleClientConnManager;
 import org.apache.http.params.HttpParams;
+import org.json.JSONObject;
 import org.junit.Test;
 
 /**
@@ -33,24 +47,120 @@ import org.junit.Test;
  * @author David
  */
 public class RESTTester {
-    
-    @Test
-    public void testCreateUser() throws UnirestException{
-        String url = "https://localhost:8080/data/post/createUser";
-//      "{"username":"jasmith","password":"asdf","type":"sequence","name":"B34 Sequence","value":"atag"}";
-    
-        HttpResponse<String> res = Unirest.post(url)
-                .header("accept", "application/json")
-                .field("username", "jasmith")
-                .field("password", "asdf")
-                .field("type","sequence")
-                .field("name", "B34 Sequence")
-                .field("value", "atag").asString();
-        
-        String body = res.getBody();
-        
-        
-        System.out.println(body);
+
+    private String url = "https://localhost:8443/data/post";
+
+    //@Test
+    public void testCreateUser() throws MalformedURLException, IOException, KeyManagementException, NoSuchAlgorithmException {
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        System.out.println("Testing Create User");
+        String jsonString = "{'username':'jsmith','password':'asdf'}";
+
+        URL url = new URL(this.url + "/create/user");
+
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Cache-Control", "no-cache");
+        conn.setInstanceFollowRedirects(false);
+
+        OutputStream os = conn.getOutputStream();
+        os.write(jsonString.getBytes());
+        os.flush();
+
+        if (conn.getResponseCode() == 200) {
+            System.out.println("SUCCESS!");
+
+            //print result
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            String alloutput = "";
+            while ((output = br.readLine()) != null) {
+                alloutput += output;
+            }
+            System.out.println(alloutput);
+            //JSONObject jsonresponse = new JSONObject(alloutput);
+            //return jsonresponse;
+        }
+        conn.disconnect();
     }
     
+    @Test
+    public void testCreateSequence() throws MalformedURLException, IOException, KeyManagementException, NoSuchAlgorithmException {
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        System.out.println("Testing Create Sequence");
+        String jsonString = "{'username':'jsmith','password':'asdf','objectName':'Test Sequence','sequence':'ata'}";
+
+        URL url = new URL(this.url + "/create/sequence");
+
+        HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+        conn.setDoInput(true);
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("Cache-Control", "no-cache");
+        conn.setInstanceFollowRedirects(false);
+
+        OutputStream os = conn.getOutputStream();
+        os.write(jsonString.getBytes());
+        os.flush();
+
+        if (conn.getResponseCode() == 200) {
+            System.out.println("SUCCESS!");
+
+            //print result
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+            String output;
+            String alloutput = "";
+            while ((output = br.readLine()) != null) {
+                alloutput += output;
+            }
+            System.out.println(alloutput);
+            //JSONObject jsonresponse = new JSONObject(alloutput);
+            //return jsonresponse;
+        }
+        conn.disconnect();
+    }
 }
